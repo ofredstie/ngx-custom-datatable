354df532bc516bce1fe49ca3d36168a4fe78be1b
Øyvind Fredstie <oyvind.fredstie@bouvet.no>
2017-11-16 15:15:59 +0100

    Added pager to component index

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ export * from './datatable.component';
 export * from './header';
 export * from './body';
 export * from './footer';
+export * from './pager';
 
 export * from './columns';
 export * from './row-detail';
